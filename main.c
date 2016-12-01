d40d4aa690436034f85955a6faf66d1467273f8e
Gabriel Vasconcelos <vasconcelos.biel@hotmail.com>
2016-12-01 12:17:49 -0200

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 int jogo_terminado = FALSE;
-int mapa[6][6] = {
+
+int mapa[6][6] = {//EX.: 0|-1| = 1 entao é possivel fazer a ligação, 0|-2| = 2 entao nao se pode fazer a ligação
+    //nas colunas é o mesmo porem tendo 6 como resultado necessario para validar a linha
     {0, 1 , 2, 3 ,4, 5},
     {6, 7, 8 ,9, 10,11},
     {12,13,14,15,16,17},
