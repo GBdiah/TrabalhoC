0e04129dedbf55b6b7231ec9b1b29b38375d70cf
Gabriel Vasconcelos <vasconcelos.biel@hotmail.com>
2016-12-04 16:24:00 -0300

    coloquei uma variavel VEZ
    usar a variavel VEZ na verificação tambem, pra trocar de jogador

--- main.c
+++ main.c
@@ -1,38 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+int player1 = 1;
+int player2 = 2;
+
+
 int jogo_terminado = 0;
 int linhas, colunas;
 int mapa[6][6] = //EX.: 0|-1| = 1 entao é possivel fazer a ligação, 0|-2| = 2 entao nao se pode fazer a ligação
     //nas colunas é o mesmo porem tendo 6 como resultado necessario para validar a linha
-    {{0, 1 , 2, 3 ,4, 5},
-    {6, 7, 8 ,9, 10,11},
+    {{0,1,2,3,4,5},
+    {06,7,8,9, 10,11},
     {12,13,14,15,16,17},
     {18,19,20,21,22,23},
     {24,25,26,27,28,29},
     {30,31,32,33,34,35}};
 
-int player1 = 1;
-int player2 = 2;
 
 
+
+
+int ponto1, ponto2;
+
+int vez = 1;
 
 int main()
 {
     while(jogo_terminado == 0) {
-    printf("----------- JOGO DOS PONTINHOS ----------\n");
-
+    printf("----------- JOGO DOS PONTINHOS ----------\n\n");
+        // imprime os valores do mapa
         int i,j;
-
         for (i = 0; i < 6; i++) {
             for (j = 0; j < 6; j++) {
-
                 printf("%d ", mapa[i][j]);
         }
-        printf("\n");
+        printf("\n\n");
+    }
+    if (vez == 1){
+    printf("------------- JOGADOR [%d] --------------\n",player1);
+    printf("Digite o ponto 1: \n");
+    scanf("%d", ponto1);
+    printf("Digite o ponto 2: \n");
+    scanf("%d", ponto2);
+    return 0;
+
+    if (vez == 2) {
+        printf("------------- JOGADOR [%d] --------------\n",player2);
+    printf("Digite o ponto 1: \n");
+    scanf("%d", ponto1);
+    printf("Digite o ponto 2: \n");
+    scanf("%d", ponto2);
+
+
+
     }
 
-    printf("------------- JOGADOR [%d] --------------\n",player1);
-    return 0;
-    }
+
+}
+
+
+}
 }
